a309c83bf8fac99e1d5bb3888172c0d7b21d580f
Naim Chowdhury <Naim.91@hotmail.com>
2023-09-17 15:34:48 +0200

    More logo images

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,13 +1,35 @@
+import fanblack from "../assets/fanblack.svg";
+import fanred from "../assets/fanred.svg";
+import fanwhite from "../assets/fanwhite.svg";
+import geishablack from "../assets/geishablack.svg";
+import geishared from "../assets/geishared.svg";
+import geishawhite from "../assets/geishawhite.svg";
+import kanjiblack from "../assets/kanjiblack.svg";
+import kanjiwhite from "../assets/kanjiwhite.svg";
 import logo from "../assets/miyabilogo.svg";
 import logo2 from "../assets/miyabilogo2.svg";
 import logoWhite from "../assets/miyabilogoWhite.svg";
 import logowhite from "../assets/logowhite.svg";
 import logoblack from "../assets/logoblack.svg";
+import miyabitextblack from "../assets/miyabitextblack.svg";
+import miyabitextred from "../assets/miyabitextred.svg";
+import miyabitextwhite from "../assets/miyabitextwhite.svg";
 
 export default {
+  fanblack,
+  fanred,
+  fanwhite,
+  geishablack,
+  geishared,
+  geishawhite,
+  kanjiblack,
+  kanjiwhite,
   logo,
   logo2,
   logoWhite,
   logowhite,
   logoblack,
+  miyabitextblack,
+  miyabitextred,
+  miyabitextwhite,
 };
