94133a381bbe69cd037506f8fa1a1dfb042b0e12
Naim Chowdhury <Naim.91@hotmail.com>
2023-09-25 11:09:31 +0200

    Added icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
+import { FaLocationDot } from "react-icons/fa6"
+import {BsTelephoneFill} from "react-icons/bs"
 import "./Header.css"
 const Header = () => {
   return (
-    <div className="header app__bg p__opensans">
-      <p>Telefon</p>
-      <a href="tel:+46763043985">076-304 39 85</a>
-      <p>Address</p>
-      <a href="https://maps.app.goo.gl/jVynK1HvdsJKrXAx5" target="_blank" rel="noreferrer">Kyrkvägen, Hersbyholms Stationshus, Lidingö</a>
-    </div>
-  )
+    <div className="app__bg p__opensans" >
+      <a className="header" href="tel:+46763043985">
+        <BsTelephoneFill />
+        <p>Telefon</p>
+        <p>076-304 39 85</p>
+      </a>
+
+      <a className="header" href="https://maps.app.goo.gl/jVynK1HvdsJKrXAx5" target="_blank" rel="noreferrer">
+        <FaLocationDot />
+        <p>Address</p>
+        <p>Kyrkvägen, Hersbyholms Stationshus, Lidingö</p>
+      </a>
+    </div >  
+    )
 }
 
 export default Header
