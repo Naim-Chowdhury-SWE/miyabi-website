762580057328f4b749643f69f3c7685c7b64ec1e
Naim Chowdhury <Naim.91@hotmail.com>
2023-10-22 20:12:13 +0200

    Added lunch times

--- src/components/Meny/Lunch/Lunch.jsx
+++ src/components/Meny/Lunch/Lunch.jsx
@@ -3,43 +3,48 @@ import MenuItem from "../../Menuitem/MenuItem";
 import "./Lunch.css";
 
 const Lunch = () => {
-    return (
-        <div className="lunchMeny app__bg flex__center section__padding" id="Meny">
-            <div className="lunchMeny-title">
-                <h1 className="headtext__cormorant">Lunch Meny</h1>
-            </div>
-            
-            <div className="lunchMeny-meny">
-                <div className="lunchMeny-meny-dagens">
-                    <p className="lunchMeny-meny-heading">Dagens Rätt</p>
-                        <div className="lunchMeny-meny-mat">
-                        {data.lunchMeny1.map((lunch, index) => (
-                            <MenuItem
-                                key={lunch.title + index}
-                                image={lunch.image}
-                                title={lunch.title}
-                                price={lunch.price}
-                                description={lunch.description} />
-                        ))}
-                        </div>    
-                </div> 
+  return (
+    <div className="lunchMeny app__bg flex__center section__padding" id="Meny">
+      <div className="lunchMeny-title">
+        <h1 className="headtext__cormorant">Lunch Meny</h1>
+        <p className="p__cormorant">
+          Lunchen gäller vardagar mellan kl <time>11:00 och 14:00</time>
+        </p>
+      </div>
 
-                <div className="lunchMeny-meny-dagens2">
-                    <p className="lunchMeny-meny-heading">Finns varje dag</p>
-                        <div className="lunchMeny-meny-mat">
-                        {data.lunchMeny2.map((lunch, index) => (
-                            <MenuItem
-                                key={lunch.title + index}
-                                image={lunch.image}
-                                title={lunch.title}
-                                price={lunch.price}
-                                description={lunch.description} />
-                        ))}
-                        </div>
-                </div>
-                
-            </div>
+      <div className="lunchMeny-meny">
+        <div className="lunchMeny-meny-dagens">
+          <p className="lunchMeny-meny-heading">Dagens Rätt</p>
+          <div className="lunchMeny-meny-mat">
+            {data.lunchMeny1.map((lunch, index) => (
+              <MenuItem
+                key={lunch.title + index}
+                image={lunch.image}
+                title={lunch.title}
+                price={lunch.price}
+                description={lunch.description}
+              />
+            ))}
+          </div>
         </div>
-)}
- 
+
+        <div className="lunchMeny-meny-dagens2">
+          <p className="lunchMeny-meny-heading">Finns varje dag</p>
+          <div className="lunchMeny-meny-mat">
+            {data.lunchMeny2.map((lunch, index) => (
+              <MenuItem
+                key={lunch.title + index}
+                image={lunch.image}
+                title={lunch.title}
+                price={lunch.price}
+                description={lunch.description}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Lunch;
