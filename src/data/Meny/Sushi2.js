f7d6cd9fa4da0f2e2cbd1d79ab50557cdf039093
Naim Chowdhury <Naim.91@hotmail.com>
2023-11-09 21:50:40 +0100

    Testing a new data format

---
+++ src/data/Meny/Sushi2.js
@@ -0,0 +1,318 @@
+import images from "../../constants/images";
+
+const blandadSushi2 = [
+  {
+    title: "Blandad Sushi",
+    variants: [
+      {
+        title: "7 bitar",
+        description: "Blandad sushi med lax, rullar och avocado",
+        price: "89kr",
+        image: images.BlandadSushi,
+      },
+      {
+        title: "8 bitar",
+        description: "Blandad sushi med lax, rullar och avocado",
+        price: "99kr",
+        image: images.BlandadSushi,
+      },
+      {
+        title: "9 bitar",
+        description: "Blandad sushi med lax, rullar, räkor och avocado",
+        price: "109kr",
+        image: images.BlandadSushi,
+      },
+      {
+        title: "11 bitar",
+        description: "Blandad sushi med lax, rullar, räkor och avocado",
+        price: "129kr",
+        image: images.BlandadSushi,
+      },
+    ],
+  },
+  {
+    title: "Blandad Sushi",
+    variants: [
+      {
+        title: "13 bitar",
+        description: "Blandad sushi med lax, rullar, räkor och avocado",
+        price: "149kr",
+        image: images.placeholderNigiri,
+      },
+      {
+        title: "14 bitar",
+        description: "Blandad sushi med lax, rullar, räkor och avocado",
+        price: "159kr",
+        image: images.placeholderNigiri,
+      },
+      {
+        title: "16 bitar",
+        description: "Blandad sushi med lax, rullar, räkor och avocado",
+        price: "179kr",
+        image: images.placeholderNigiri,
+      },
+      {
+        title: "20 bitar",
+        description: "Blandad sushi med lax, rullar, räkor och avocado",
+        price: "219kr",
+        image: images.placeholderNigiri,
+      },
+    ],
+  },
+];
+
+const menuSushi2 = [
+  {
+    title: "Lax & Rullar Sushi",
+    variants: [
+      {
+        title: "8 bitar",
+        description: "Hälften lax & hälften rullar",
+        price: "99kr",
+        image: images.placeholderNigiri,
+      },
+      {
+        title: "10 bitar",
+        description: "Hälften lax & hälften rullar",
+        price: "119kr",
+        image: images.placeholderNigiri,
+      },
+      {
+        title: "12 bitar",
+        description: "Hälften lax & hälften rullar",
+        price: "139kr",
+        image: images.placeholderNigiri,
+      },
+    ],
+  },
+  {
+    title: "Vegetarisk / Vegansk Sushi",
+    variants: [
+      {
+        title: "10 bitar",
+        description:
+          "Blandad sushi som innehåller Vegetariska rullar, Avocado och Tofu",
+        price: "109kr",
+        image: images.placeholderNigiri,
+      },
+      {
+        title: "12 bitar",
+        description:
+          "Blandad sushi som innehåller Vegetariska rullar, Avocado och Tofu",
+        price: "129kr",
+        image: images.placeholderNigiri,
+      },
+    ],
+  },
+  {
+    title: "Mamma Sushi",
+    variants: [
+      {
+        title: "10 bitar",
+        description: "Blandad sushi men utan råfisk",
+        price: "109kr",
+        image: images.placeholderNigiri,
+      },
+      {
+        title: "12 bitar",
+        description: "Blandad sushi men utan råfisk",
+        price: "135kr",
+        image: images.placeholderNigiri,
+      },
+    ],
+  },
+  {
+    title: "9 bitar Blandad Nigiri (utan rullar)",
+    variants: [
+      {
+        title: "9 bitar",
+        description: "Endast blandat med olika fisksorter",
+        price: "119kr",
+        image: images.placeholderNigiri,
+      },
+    ],
+  },
+  {
+    title: "12 bitar Blandad Nigiri (utan rullar)",
+    variants: [
+      {
+        title: "12 bitar",
+        description: "Endast blandat med olika fisksorter",
+        price: "150kr",
+        image: images.placeholderNigiri,
+      },
+    ],
+  },
+  {
+    title: "Lax Sushi",
+    variants: [
+      {
+        title: "8 bitar",
+        description: "Laxsushi",
+        price: "120kr",
+        image: images.LaxSushi,
+      },
+      {
+        title: "10 bitar",
+        description: "Laxsushi",
+        price: "140kr",
+        image: images.LaxSushi,
+      },
+      {
+        title: "12 bitar",
+        description: "Laxsushi",
+        price: "160kr",
+        image: images.LaxSushi,
+      },
+    ],
+  },
+  {
+    title: "Lax & Avocado Sushi",
+    variants: [
+      {
+        title: "8 bitar",
+        description: "Hälften lax och hälften avocado",
+        price: "115kr",
+        image: images.LaxAvocadoSushi,
+      },
+      {
+        title: "10 bitar",
+        description: "Hälften lax och hälften avocado",
+        price: "125kr",
+        image: images.LaxAvocadoSushi,
+      },
+      {
+        title: "12 bitar",
+        description: "Hälften lax och hälften avocado",
+        price: "145kr",
+        image: images.LaxAvocadoSushi,
+      },
+    ],
+  },
+
+  {
+    title: "Lax & Räkor Sushi",
+    variants: [
+      {
+        title: "8 bitar",
+        description: "Hälften lax & hälften räkor",
+        price: "115kr",
+        image: images.LaxRäkorSushi,
+      },
+      {
+        title: "10 bitar",
+        description: "Hälften lax & hälften räkor",
+        price: "130kr",
+        image: images.LaxRäkorSushi,
+      },
+      {
+        title: "12 bitar",
+        description: "Hälften lax & hälften räkor",
+        price: "150kr",
+        image: images.LaxRäkorSushi,
+      },
+    ],
+  },
+  {
+    title: "Avocado Sushi",
+    variants: [
+      {
+        title: "8 bitar",
+        description: "Avocadosushi",
+        price: "105kr",
+        image: images.AvocadoSushi,
+      },
+      {
+        title: "10 bitar",
+        description: "Avocadosushi",
+        price: "125kr",
+        image: images.AvocadoSushi,
+      },
+      {
+        title: "12 bitar",
+        description: "Avocadosushi",
+        price: "140kr",
+        image: images.AvocadoSushi,
+      },
+    ],
+  },
+  {
+    title: "Räksushi",
+    variants: [
+      {
+        title: "8 bitar",
+        description: "Sushiräkor",
+        price: "120kr",
+        image: images.placeholderNigiri,
+      },
+      {
+        title: "10 bitar",
+        description: "Sushiräkor",
+        price: "139kr",
+        image: images.placeholderNigiri,
+      },
+      {
+        title: "12 bitar",
+        description: "Sushiräkor",
+        price: "160kr",
+        image: images.placeholderNigiri,
+      },
+    ],
+  },
+  {
+    title: "Grillad Lax med chilimajo",
+    variants: [
+      {
+        title: "8 bitar",
+        description: "Grillad laxsushi som toppas med chilimajonäs",
+        price: "135kr",
+        image: images.GrillLaxSushi,
+      },
+      {
+        title: "10 bitar",
+        description: "Grillad laxsushi som toppas med chilimajonäs",
+        price: "155kr",
+        image: images.GrillLaxSushi,
+      },
+      {
+        title: "12 bitar",
+        description: "Grillad laxsushi som toppas med chilimajonäs",
+        price: "175kr",
+        image: images.GrillLaxSushi,
+      },
+    ],
+  },
+];
+
+const familjeSushi2 = [
+  {
+    title: "Familjesushi 30 bitar",
+    description:
+      "En stor blandad sushi med alla möjliga sushityper vi erbjuder",
+    price: "329kr",
+    image: images.FamiljeSushiSmall,
+  },
+  {
+    title: "Familjesushi 40 bitar",
+    description:
+      "En stor blandad sushi med alla möjliga sushityper vi erbjuder",
+    price: "429kr",
+    image: images.FamiljeSushiMedium,
+  },
+  {
+    title: "Familjesushi 50 bitar",
+    description:
+      "En stor blandad sushi med alla möjliga sushityper vi erbjuder",
+    price: "529kr",
+    image: images.FamiljeSushiMedium,
+  },
+  {
+    title: "Familjesushi 70 bitar",
+    description:
+      "En stor blandad sushi med alla möjliga sushityper vi erbjuder",
+    price: "729kr",
+    image: images.placeholderNigiri,
+  },
+];
+
+export default { blandadSushi2, menuSushi2, familjeSushi2 };
