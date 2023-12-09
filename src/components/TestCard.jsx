47cde28d09323a7fdc06a89eb16436f7ff366dc3
Naim Chowdhury <Naim.91@hotmail.com>
2023-12-09 11:34:18 +0100

    Testing array data structures

--- src/components/TestCard.jsx
+++ src/components/TestCard.jsx
@@ -34,12 +34,93 @@ TestCard.propTypes = {
 
 export default TestCard; */
 
-import SushiData from "../data/Meny/Sushi2";
+const dummyArray1 = ["David", "Lee", "Jenna"];
+
+const dummyArray2 = [
+  {
+    name: "David",
+    age: 32,
+    job: "Marketing",
+  },
+  {
+    name: "Lee",
+    age: 16,
+    job: "Coding",
+  },
+  {
+    name: "Jenna",
+    age: 22,
+    job: "Designing",
+  },
+];
+
+const dummyArray3 = [
+  {
+    title: "Lax Sushi",
+    description: "Färsk lax sushi med ris, wasabi och ingefära",
+    variants: [
+      {
+        subtitle: "8 bitar",
+        price: "100kr",
+      },
+      {
+        subtitle: "10 bitar",
+        price: "120kr",
+      },
+      {
+        subtitle: "12 bitar",
+        price: "140kr",
+      },
+    ],
+  },
+  {
+    title: "Avocado Sushi",
+    description: "Färsk avocado sushi med ris, sjögräss och ingefära",
+    variants: [
+      {
+        subtitle: "8 bitar",
+        price: "77kr",
+      },
+      {
+        subtitle: "10 bitar",
+        price: "88kr",
+      },
+      {
+        subtitle: "12 bitar",
+        price: "99kr",
+      },
+    ],
+  },
+];
 
 const TestCard = () => {
   return (
     <div>
-      <h1>Sushi Menu</h1>
+      <h1>DummyArray 1</h1>
+      {dummyArray1.map((name) => (
+        <p key={name}>{name}</p>
+      ))}
+      <h1>DummyArray 2</h1>
+      {dummyArray2.map((person) => (
+        <>
+          <h3>{person.name}</h3>
+          <p>{person.age}</p>
+          <p>{person.job}</p>
+        </>
+      ))}
+      <h1>DummyArray 3</h1>
+      {dummyArray3.map((item) => (
+        <div key={item.title}>
+          <h2>{item.title}</h2>
+          <p>{item.description}</p>
+          {item.variants.map((variant) => (
+            <div key={variant.subtitle}>
+              <h3>{variant.subtitle}</h3>
+              <p>{variant.price}</p>
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
