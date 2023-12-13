a8316d4bc202c75bb57424b537b760341e022e76
Naim Chowdhury <Naim.91@hotmail.com>
2023-12-13 19:51:04 +0100

    [style] Went back to dark background

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,8 @@
-import { FaLocationDot } from "react-icons/fa6";
-import { BsTelephoneFill } from "react-icons/bs";
 import "./Header.css";
 const Header = () => {
   return (
     <header className="app__bg header">
-      <h1 className="headtext__cormorant">Välkommen till Miyabi</h1>
+      <h1 className="header-head">Välkommen till Miyabi</h1>
       <div className="p__cormorant">
         <p className="header-p">
           Miyabi serverar spännande mat från det japanska köket veckans alla
@@ -17,22 +15,6 @@ const Header = () => {
           Kolla in vår nya meny för <span className="gold">2024</span>
         </p>
       </div>
-      {/* <a className="header" href="tel:+46763043985">
-        <BsTelephoneFill />
-        <p>Telefon</p>
-        <p>076-304 39 85</p>
-      </a>
-
-      <a
-        className="header"
-        href="https://maps.app.goo.gl/jVynK1HvdsJKrXAx5"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <FaLocationDot />
-        <p>Address</p>
-        <p>Kyrkvägen, Hersbyholms Stationshus, Lidingö</p>
-      </a> */}
     </header>
   );
 };
